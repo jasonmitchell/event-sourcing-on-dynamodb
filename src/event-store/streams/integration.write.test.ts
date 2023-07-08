07b58bf39209d7726a3452d8f00c2f19b5e8efe1
Jason Mitchell <jasonmitchell@live.co.uk>
2023-07-08 22:56:41 +0100

    Implement new transactional write function

---
+++ src/event-store/streams/integration.write.test.ts
@@ -0,0 +1,205 @@
+import { connect, EventStore } from '../index';
+import { randomUUID } from 'crypto';
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { EventRecord } from './events';
+import { readStream, ReadStreamOptions } from './read';
+import { createTable, randomEvents, randomMetadata } from './integration.sdk';
+import { writeStream } from './write';
+
+describe('Event Store', () => {
+  const dynamoDB = new DynamoDB({ region: 'eu-west-1', endpoint: 'http://localhost:8100' });
+  let tableName = `test-${new Date().getTime()}`;
+  let eventStore: EventStore;
+  let defaultOptions = { partitionSize: 10, dynamoDB, tableName };
+
+  beforeEach(async () => {
+    tableName = `test-${new Date().getTime()}`;
+    defaultOptions = { partitionSize: 10, dynamoDB, tableName };
+
+    eventStore = connect({
+      client: dynamoDB,
+      tableName: tableName
+    });
+
+    await createTable(dynamoDB, tableName);
+  });
+
+  describe('write stream', () => {
+    it('appends single event to stream', async () => {
+      const streamId = `write-stream-${randomUUID()}`;
+      const events = randomEvents(1);
+      const result = await writeStream(streamId, events, defaultOptions);
+      const retrievedEvents = await readEventsFromStream(streamId);
+
+      expect.assertions(1);
+      if (result.success) {
+        expect(retrievedEvents).toEqual(result.records);
+      }
+    });
+
+    it('appends events to stream', async () => {
+      const streamId = `write-stream-${randomUUID()}`;
+      const events = randomEvents(3);
+      const result = await writeStream(streamId, events, defaultOptions);
+      const retrievedEvents = await readEventsFromStream(streamId);
+
+      expect.assertions(1);
+      if (result.success) {
+        expect(retrievedEvents).toEqual(result.records);
+      }
+    });
+
+    it('cannot write to streams beginning with the reserved streams prefix ($)', async () => {
+      const streamId = `$write-stream-${randomUUID()}`;
+      const events = randomEvents(3);
+      const result = await writeStream(streamId, events, defaultOptions);
+
+      expect.assertions(1);
+      if (!result.success) {
+        expect(result.error).toEqual({
+          type: 'invalid_stream_id',
+          detail: `Cannot write to stream ${streamId}, the prefix $ is reserved for internal streams`
+        });
+      }
+    });
+
+    describe('expected versions', () => {
+      describe('any version', () => {
+        it('writes to stream when it does not exist', async () => {
+          const streamId = `write-stream-${randomUUID()}`;
+          const events = randomEvents(3);
+          const result = await writeStream(streamId, events, { ...defaultOptions, expectedVersion: 'any' });
+
+          expect.assertions(1);
+          if (result.success) {
+            const retrievedEvents = await readEventsFromStream(streamId);
+            expect(retrievedEvents).toEqual(result.records);
+          }
+        });
+
+        it('writes to stream when it already exists', async () => {
+          const streamId = `write-stream-${randomUUID()}`;
+          const [_, seed] = await seedStreamAtVersion(streamId, 3);
+
+          const events = randomEvents(3);
+          const result = await writeStream(streamId, events, { ...defaultOptions, expectedVersion: 'any' });
+
+          expect.assertions(1);
+          if (result.success) {
+            const retrievedEvents = await readEventsFromStream(streamId);
+            expect(retrievedEvents).toEqual([...seed, ...result.records]);
+          }
+        });
+      });
+
+      describe('no stream', () => {
+        it('writes to a stream when it does not exist', async () => {
+          const streamId = `write-stream-${randomUUID()}`;
+          const events = randomEvents(3);
+          const result = await writeStream(streamId, events, { ...defaultOptions, expectedVersion: 'no_stream' });
+
+          expect.assertions(1);
+          if (result.success) {
+            const retrievedEvents = await readEventsFromStream(streamId);
+            expect(retrievedEvents).toEqual(result.records);
+          }
+        });
+
+        it('cannot write to a stream when it already exists', async () => {
+          const streamId = `write-stream-${randomUUID()}`;
+          await seedStreamAtVersion(streamId, 3);
+
+          const events = randomEvents(3);
+          const result = await writeStream(streamId, events, { ...defaultOptions, expectedVersion: 'no_stream' });
+
+          expect.assertions(1);
+          if (!result.success) {
+            expect(result.error).toEqual({
+              type: 'stream_version_mismatch',
+              detail: `Expected stream ${streamId} not to exist, but it does`
+            });
+          }
+        });
+      });
+
+      describe('specific version', () => {
+        it('writes to a stream when it is at the specified version', async () => {
+          const streamId = `write-stream-${randomUUID()}`;
+          const [version, seed] = await seedStreamAtVersion(streamId, 3);
+
+          const events = randomEvents(3);
+          const result = await writeStream(streamId, events, { ...defaultOptions, expectedVersion: version });
+
+          expect.assertions(1);
+          if (result.success) {
+            const retrievedEvents = await readEventsFromStream(streamId);
+            expect(retrievedEvents).toEqual([...seed, ...result.records]);
+          }
+        });
+
+        it('cannot write to a stream when it is at a newer version than expected', async () => {
+          const streamId = `write-stream-${randomUUID()}`;
+          const [version] = await seedStreamAtVersion(streamId, 3);
+
+          const events = randomEvents(3);
+          const result = await writeStream(streamId, events, { ...defaultOptions, expectedVersion: version - 1 });
+
+          expect.assertions(1);
+          if (!result.success) {
+            expect(result.error).toEqual({
+              type: 'stream_version_mismatch',
+              detail: `Expected stream ${streamId} to be at version ${version - 1}, but it was at version ${version}`
+            });
+          }
+        });
+
+        it('cannot write to a stream when it is at an older version than expected', async () => {
+          const streamId = `write-stream-${randomUUID()}`;
+          const [version] = await seedStreamAtVersion(streamId, 3);
+
+          const events = randomEvents(3);
+          const result = await writeStream(streamId, events, { ...defaultOptions, expectedVersion: version + 1 });
+
+          expect.assertions(1);
+          if (!result.success) {
+            expect(result.error).toEqual({
+              type: 'stream_version_mismatch',
+              detail: `Expected stream ${streamId} to be at version ${version + 1}, but it was at version ${version}`
+            });
+          }
+        });
+      });
+    });
+  });
+
+  const seedStreamAtVersion = async (streamId: string, version: number): Promise<[number, EventRecord[]]> => {
+    const metadata = randomMetadata();
+    const events = randomEvents(version + 1).map(event => ({
+      id: randomUUID(),
+      type: 'TestEvent',
+      data: event,
+      metadata: metadata
+    }));
+
+    const result = await writeStream(streamId, events, { ...defaultOptions, expectedVersion: 'no_stream' });
+    if (!result.success) {
+      throw Error(`Failed to seed stream ${streamId} at version ${version}`);
+    }
+
+    return [version, result.records];
+  };
+
+  const readEventsFromStream = async (streamId: string): Promise<EventRecord[]> => {
+    const readStreamOptions: ReadStreamOptions = {
+      dynamoDB,
+      tableName
+    };
+
+    const events = [];
+    for await (const event of readStream(streamId, readStreamOptions)) {
+      events.push(event);
+    }
+
+    return events;
+  };
+});
---
+++ src/event-store/streams/write.ts
@@ -0,0 +1,170 @@
+import { EventStoreOptions } from '../types';
+import { Event, EventRecord } from './events';
+import { readStream, ReadStreamOptions } from './read';
+import { getNextEventPosition } from '../event-position';
+import { AttributeValue } from '@aws-sdk/client-dynamodb/dist-types/models/models_0';
+
+export type ExpectedVersion = 'any' | 'no_stream' | number;
+
+export type WriteStreamOptions = EventStoreOptions & {
+  partitionSize: number;
+  expectedVersion?: ExpectedVersion;
+};
+
+export type WriteStreamResult = SuccessfulWriteStreamResult | ErroredWriteStreamResult;
+
+type SuccessfulWriteStreamResult = {
+  success: true;
+  records: EventRecord[];
+};
+
+type ErroredWriteStreamResult = {
+  success: false;
+  error: {
+    type: string;
+    detail: string;
+  };
+};
+
+export const writeStream = async (streamId: string, events: Event[], options: WriteStreamOptions): Promise<WriteStreamResult> => {
+  const expectedVersion: ExpectedVersion = options.expectedVersion || 'any';
+  const streamVersion = await getStreamVersion(streamId, options);
+  const error = await validateWriteRequest(streamId, expectedVersion, streamVersion);
+  if (error) {
+    return error;
+  }
+
+  const records = await getEventRecords(streamId, events, streamVersion, options);
+  await writeToDynamo(streamId, records, options);
+
+  return {
+    success: true,
+    records: records
+  };
+};
+
+const validateWriteRequest = (
+  streamId: string,
+  expectedVersion: ExpectedVersion,
+  streamVersion: number
+): ErroredWriteStreamResult | null => {
+  if (streamId.startsWith('$')) {
+    return {
+      success: false,
+      error: {
+        type: 'invalid_stream_id',
+        detail: `Cannot write to stream ${streamId}, the prefix $ is reserved for internal streams`
+      }
+    };
+  }
+
+  if (expectedVersion !== 'any') {
+    return validateStreamVersion(streamId, expectedVersion, streamVersion);
+  }
+
+  return null;
+};
+
+const validateStreamVersion = (
+  streamId: string,
+  expectedVersion: ExpectedVersion,
+  streamVersion: number
+): ErroredWriteStreamResult | null => {
+  if (expectedVersion === 'no_stream' && streamVersion >= 0) {
+    return {
+      success: false,
+      error: {
+        type: 'stream_version_mismatch',
+        detail: `Expected stream ${streamId} not to exist, but it does`
+      }
+    };
+  }
+
+  if (expectedVersion !== 'any' && expectedVersion !== 'no_stream' && streamVersion !== expectedVersion) {
+    return {
+      success: false,
+      error: {
+        type: 'stream_version_mismatch',
+        detail: `Expected stream ${streamId} to be at version ${expectedVersion}, but it was at version ${streamVersion}`
+      }
+    };
+  }
+
+  return null;
+};
+
+const getStreamVersion = async (streamId: string, options: EventStoreOptions): Promise<number> => {
+  const readStreamOptions: ReadStreamOptions = {
+    ...options,
+    limit: 1,
+    direction: 'backward'
+  };
+
+  const streamReader = await readStream(streamId, readStreamOptions);
+  const lastEvent = await streamReader[Symbol.asyncIterator]().next();
+  return lastEvent?.value?.version || -1;
+};
+
+const getEventRecords = async (
+  streamId: string,
+  events: Event[],
+  streamVersion: number,
+  options: WriteStreamOptions
+): Promise<EventRecord[]> => {
+  const { dynamoDB, tableName, partitionSize } = options;
+  const createdAt = new Date().toISOString();
+  const startEventPosition = await getNextEventPosition(dynamoDB, tableName, events.length);
+
+  return events.map((event, index) => {
+    const version = streamVersion + index + 1;
+    const eventPosition = startEventPosition + index;
+    const eventPartition = Math.floor(eventPosition / partitionSize);
+
+    return {
+      ...event,
+      stream_id: streamId,
+      metadata: event.metadata || {},
+      version,
+      created_at: createdAt,
+      event_partition: `partition#${eventPartition}`,
+      event_position: eventPosition
+    };
+  });
+};
+
+const writeToDynamo = async (streamId: string, events: EventRecord[], options: EventStoreOptions) => {
+  const { dynamoDB, tableName } = options;
+
+  const conditionExpression =
+    '(attribute_not_exists(pk) AND attribute_not_exists(sk)) OR (attribute_exists(pk) AND attribute_not_exists(sk))';
+
+  const items: Record<string, AttributeValue>[] = events.map(event => ({
+    pk: { S: streamId },
+    sk: { N: event.version.toString() },
+    event_partition: { S: event.event_partition },
+    event_position: { N: event.event_position.toString() },
+    event_id: { S: event.id },
+    event_type: { S: event.type },
+    created_at: { S: event.created_at },
+    data: { S: JSON.stringify(event.data) },
+    metadata: { S: JSON.stringify(event.metadata) }
+  }));
+
+  if (items.length > 1) {
+    await dynamoDB.transactWriteItems({
+      TransactItems: items.map(item => ({
+        Put: {
+          TableName: tableName,
+          ConditionExpression: conditionExpression,
+          Item: item
+        }
+      }))
+    });
+  } else {
+    await dynamoDB.putItem({
+      TableName: tableName,
+      ConditionExpression: conditionExpression,
+      Item: items[0]
+    });
+  }
+};
